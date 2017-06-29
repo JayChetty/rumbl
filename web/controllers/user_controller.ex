35f339707f56d57994955d8bfd8f7fe2f1fe62d0
Jay Chetty <jaypaulchetty@gmail.com>
2017-06-29 17:15:49 +0100

    adding users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,12 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  alias Rumbl.User
   require Logger
+
+  def new(conn, _params) do
+    changeset= User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
@@ -9,5 +15,13 @@ defmodule Rumbl.UserController do
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
   end
 end
