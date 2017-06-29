0ef20b076c85a34434a18f48153bf4b889d545a7
Jay Chetty <jaypaulchetty@gmail.com>
2017-06-29 11:37:02 +0100

    first commit

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,13 @@
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+  require Logger
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumbl.User, id)
+    render conn, "show.html", user: user
+  end
+
+  def index(conn, _params) do
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
+  end
+end
